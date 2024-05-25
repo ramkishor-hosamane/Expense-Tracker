2f1c8c0708cc8a73ffe9140183aa9ebeda2a31d9
Ram <ramhosamane@gmail.com>
2024-05-25 19:58:09 +0530

    fix path of backend

--- deployment/init_server.sh
+++ deployment/init_server.sh
@@ -19,7 +19,7 @@ sudo apt install python3-virtualenv
 virtualenv venv
 source venv/bin/activate
 
-cd backend
+cd /home/ubuntu/Expense-Tracker/backend
 # Install project dependencies
 pip install -r requirements.txt
 
