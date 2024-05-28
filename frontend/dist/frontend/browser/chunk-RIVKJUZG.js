07b3a1b8cfb0a96b9f9a496231cab014e279818f
Ram <ramhosamane@gmail.com>
2024-05-28 16:35:29 +0530

    changed settings to prod

---
+++ frontend/dist/frontend/browser/chunk-RIVKJUZG.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-AQG46VUF.js";import"./chunk-Y7MNBN42.js";export{a as ExpenseModule};
--- frontend/dist/frontend/browser/chunk-X2JGUR2M.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-55EF6TYI.js";import"./chunk-AXSZPWDI.js";export{a as ExpenseModule};
